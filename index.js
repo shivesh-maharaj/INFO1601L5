8f747d70640f37411afdf96d428bab35ac196ecc
shivesh-jm <shivjm28@gmail.com>
2026-03-07 20:39:26 -0800

    Lab 5 Exercise

--- index.js
+++ index.js
@@ -1,5 +1,109 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
+// object literal
+let bob = {
+  fname: "bob",
+  lname: "smith",
+  age: 18,
+  height: 6,
+  transcript:[
+    {
+      course: 'INFO 1603',
+      grades: [ 89, 34, 67 ]
+    },
+    {
+      course: 'INFO 1601',
+      grades: [ 89, 34, 67 ]
+    }
+  ]
+};
 
-for(let i=0; i< arr.length; i++){
-  console.log(arr[i])
+let sally = {
+  fname: "sally",
+  lname: "smith",
+  age: 18,
+  height: 6,
+  transcript:[
+    {
+      course: 'INFO 1601',
+      grades: [ 100, 89, 79 ]
+    }
+  ]
+};
+
+let paul = {
+  fname: "paul",
+  lname: "smith",
+  age: 18,
+  height: 6,
+  transcript:[
+    {
+      course: 'INFO 1600',
+      grades: [ 89, 34, 67 ]
+    }
+  ]
+};
+
+const students = [bob, sally, paul];
+
+function getAverageGrade(student, course) {
+
+  for (let i = 0; i < student.transcript.length; i++) {
+
+    if (student.transcript[i].course === course) {
+
+      let grades = student.transcript[i].grades;
+      let sum = 0;
+
+      for (let j = 0; j < grades.length; j++) {
+        sum += grades[j];
+      }
+
+      return sum / grades.length;
+    }
+  }
+
+  return -1;
 }
+
+
+function getAssignmentMark(student, course, num) {
+
+  for (let i = 0; i < student.transcript.length; i++) {
+
+    if (student.transcript[i].course === course) {
+
+      let grades = student.transcript[i].grades;
+
+      if (num >= 0 && num < grades.length) {
+        return grades[num];
+      }
+    }
+  }
+
+  return -1;
+}
+
+function averageAssessment(students, courseName, assignment) {
+
+  let sum = 0;
+  let count = 0;
+
+  for (let i = 0; i < students.length; i++) {
+
+    let mark = getAssignmentMark(students[i], courseName, assignment);
+
+    if (mark !== -1) {
+      sum += mark;
+      count++;
+    }
+  }
+
+  if (count === 0) {
+    return -1;
+  }
+
+  return sum / count;
+}
+
+console.log(getAverageGrade(bob, "INFO 1603"));
+console.log(getAssignmentMark(sally, "INFO 1601", 1));
+console.log(averageAssessment(students, "INFO 1601", 0));
